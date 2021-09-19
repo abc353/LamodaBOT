fdf011d3b0152b78d2813fea7926ede7806288db
abc353 <buka353@gmail.com>
2021-09-19 23:49:04 +0300

    make it better

--- bot.py
+++ bot.py
@@ -3,26 +3,41 @@ import constants
 from telebot import types
 bot = telebot.TeleBot(constants.token)
 main_menu = types.ReplyKeyboardMarkup(False, True, False)
-main_menu.row('Контактная информация')
-main_menu.row('Правила работы с отменами и переносами')
-otmena = types.ReplyKeyboardMarkup(False)
-otmena.row('Как мне работать с отменами и переносами?')
-otmena.row('Когда можно отменить заказ?')
-otmena.row('Главное меню')
-@bot.message_handler(commands=('start'))
+main_menu.row('Контактная информация', 'Где мой промокод')
+main_menu.row('Соблюдай правила')
+menu_pravila = types.ReplyKeyboardMarkup(False)
+menu_pravila.row('Как мне работать с отменами и переносами?')
+menu_pravila.row('Когда можно отменить заказ?', 'Я не успеваю в интервал?')
+menu_pravila.row()
+menu_pravila.row('Главное меню')
+menu_stop = types.ReplyKeyboardRemove()
+@bot.message_handler(commands='start')
 def welcome(message):
     bot.send_message(message.chat.id, 'Добро пожаловать с Ламода БОТ', reply_markup=main_menu)
-@bot.message_handler(content_types=('text'))
+#@bot.message_handler(commands='gohome')
+#def handle_text(message):
+#    kuda = types.InlineKeyboardMarkup()
+#    kuda_button = types.InlineKeyboardButton(text="Склад", callback_data="test")
+#    kuda.add(kuda_button)
+#    bot.send_message(message.chat.id, 'Построить маршрут:', reply_markup=kuda)
+
+@bot.message_handler(content_types='text')
 def handle_text(message):
     if message.text == "Контактная информация":
-        bot.send_message(message.chat.id, "Кол Центр +74995004959\nДежурный СВ+79160558030\nДежурный механик +79150110787\nДежурный кассир +79175805523\nПаркоматика  +78003015748\nМокка +78007077236\niboxPro +74955055045 +78003334526\nСБ +79999991489")
-    elif message.text == "Правила работы с отменами и переносами":
-        bot.send_message(message.chat.id, "Правила работы с отменами и переносами", reply_markup=otmena)
+        bot.send_message(message.chat.id, constants.contact, parse_mode="Markdown", reply_markup=menu_stop)
+    elif message.text == "Где мой промокод":
+        bot.send_message(message.chat.id, constants.promokod, parse_mode="Markdown", reply_markup=menu_stop)
+    elif message.text == "Соблюдай правила":
+        bot.send_message(message.chat.id, "Соблюдай правила", reply_markup=menu_pravila)
     elif message.text == "Как мне работать с отменами и переносами?":
-        bot.send_message(message.chat.id, "1 - Выясняем причину\n2 - Предлагаем доставку заказа в другой интервал сегодня же\n3 - Предлагаем доставку по другому адресу, главное чтобы клиент принял сегодня. Адрес согласовываем с супервайзером")
+            bot.send_message(message.chat.id, constants.perenos, parse_mode="Markdown")
     elif message.text == "Когда можно отменить заказ?":
-        bot.send_message(message.chat.id, "1 - Отмена по просьбе клиента (крайне желательно подтверждение отмены по рабочему телефону в звонке\n2 - У заказа кончаются попытки доставки max3( смотрим через статус недозвон или в маршрутнике\n2.1 - У заказа кончаются дни хранения на складе max5\n3 - Фактический адрес доставки не входит в зону Южного склада ( уточняем у супервайзера). Только оформление нового заказа с корректным адресом. Перенос невозможен")
+            bot.send_message(message.chat.id, constants.otmena, parse_mode="Markdown")
+    elif message.text == "Я не успеваю в интервал?":
+            bot.send_message(message.chat.id, constants.opozdanie, parse_mode="Markdown")
     elif message.text == "Главное меню":
-        bot.send_message(message.chat.id, "Главное меню", reply_markup=main_menu)
+            bot.send_message(message.chat.id, "Главное меню", reply_markup=main_menu)
     else: bot.send_message(message.chat.id, "пиши /start")
+
+
 bot.polling()
