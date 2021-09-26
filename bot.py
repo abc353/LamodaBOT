9fc59a388f5c66cb939d28cd0fa0260489978b9a
abc353 <buka353@gmail.com>
2021-09-26 21:42:16 +0300

    make it better

--- bot.py
+++ bot.py
@@ -99,6 +99,9 @@ def handle_text(message):
             bot.reply_to(message, "*Позвони в службу поддержки iBox +78003334526*", parse_mode="Markdown")
         elif "срочно" in message.text.lower():
             bot.send_message(message.chat.id, "*Запросы срочно обрабатываются в течение 5мин*", parse_mode="Markdown", disable_notification=True)
+        elif "балл" in message.text.lower():
+            bot.send_message(message.chat.id, "[Баллы Август Июль](https://docs.google.com/spreadsheets/d/1tFo0Fat2gachSWIWZKkqN_VU1xa7EhvuDmgMOewIzVg/edit#gid=1648712497)", parse_mode="Markdown")
+            bot.send_message(message.chat.id, "[Баллы Сентябрь](https://docs.google.com/spreadsheets/d/1-X9T4CkT8GP9xkLEiqj9IcX-gfS4AL_s1FNKO8m_ncQ/edit#gid=1127930766)", parse_mode="Markdown")
         elif message.text.lower() == "спасибо":
             bot.delete_message(message.chat.id, message.id)
         elif "0 перенос" in message.text.lower() and "0 отмен" in message.text.lower() and "0 недоз" in message.text.lower():
@@ -118,6 +121,12 @@ def handle_text(message):
             bot.reply_to(message, "Группируемся по 4 человека в одном направлении, доступно 3 машины", parse_mode="Markdown")
         elif "дежурн" in message.text.lower():
             bot.reply_to(message, "*Дежурный СВ* +79160558030", parse_mode="Markdown")
+        elif "перен" in message.text.lower():
+            bot.reply_to(message, "Проработай переносы!")
+            bot.send_message(message.chat.id, constants.perenos, parse_mode="Markdown")
+        elif "отмен" in message.text.lower():
+            bot.reply_to(message, "Проработай отмены!, выясни причину!")
+            bot.send_message(message.chat.id, constants.otmena, parse_mode="Markdown")
     elif message.chat.type == 'group':
         if "работает" in message.text.lower() and "айбокс" in message.text.lower():
             bot.reply_to(message, "*Позвони в службу поддержки iBox +78003334526*", parse_mode="Markdown")
